edb88bbabeb69b17c5f34e28a1f2e84d8d850453
mpkorstanje <rien.korstanje@gmail.com>
2016-08-24 21:44:24 +0200

    Updated license-maven-plugin to use non-formatting java doc tag for header

--- simmetrics-core/src/main/java/org/simmetrics/ListDistance.java
+++ simmetrics-core/src/main/java/org/simmetrics/ListDistance.java
@@ -1,4 +1,4 @@
-/*
+/*-
  * #%L
  * Simmetrics Core
  * %%
@@ -17,6 +17,7 @@
  * limitations under the License.
  * #L%
  */
+
 package org.simmetrics;
 
 import java.util.Collections;
--- simmetrics-example/src/main/java/org/simmetrics/example/ReadMeExample.java
+++ simmetrics-example/src/main/java/org/simmetrics/example/ReadMeExample.java
@@ -1,4 +1,4 @@
-/*
+/*-
  * #%L
  * Simmetrics Examples
  * %%
